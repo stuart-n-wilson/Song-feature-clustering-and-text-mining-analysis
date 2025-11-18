1f79cd76d509ab2040cffd29cf621368fefb8643
Stuart Wilson <stuwilson98@gmail.com>
2025-11-18 11:42:06 +0000

    Add files via upload

--- main.R
+++ main.R
@@ -1,14 +1,28 @@
 # Libraries ---------------------------------------------------------------
-library(tidyverse) # Wrangling
-library(patchwork) # Vis
-library(gt) # Vis
-library(factoextra) # ML
-library(reshape2) # Wrangling
-library(viridis) # Vis
-library(tm) # Text mining
-library(textclean) # Text mining
-library(textstem) # Text mining
 
+# Wrangling
+library(tidyverse)
+library(reshape2)
+library(fastmatch)
+
+# Machine Learning
+library(factoextra)
+
+# Visualisation
+library(patchwork) 
+library(gt)
+library(viridis)
+library(ggwordcloud)
+
+# Text mining
+library(tm)
+library(textclean)
+library(textstem)
+library(tidytext)
+library(qdapDictionaries)
+
+
+# Importing data ----------------------------------------------------------
 
 # Files should be in working directory.
 acoustic_features <- read.delim(
@@ -58,7 +72,6 @@ df <- df |>
     mode = as.factor(mode),
     time_signature = as.factor(time_signature)
   )
-
 
 # Feature visualisation ---------------------------------------------------
 
@@ -220,7 +233,7 @@ p_acou + p_dance + p_energy + p_instru + p_live + p_loud + p_speech +
 
 # We consider only the numeric values, as this will be for the model.
 df_numeric <- df |>
-  select(!c("song_id", "key", "mode", "time_signature", "lyrics"))
+  select(-song_id, -key, -mode, -time_signature, -lyrics)
 
 corr_matrix <- cor(df_numeric, method = "spearman")
 
@@ -254,14 +267,12 @@ ggplot(df, aes(x = loudness, y = energy, colour = acousticness)) +
   labs(title = "Loudness energy correlation") +
   theme(legend.key.height = unit(3, "cm"))
 
-
 ggplot(df, aes(x = acousticness, y = energy, colour = loudness)) +
   geom_point() +
   theme_minimal() +
   scale_colour_viridis_c() +
   labs(title = "Acousticness energy correlation") +
   theme(legend.key.height = unit(3, "cm"))
-
 
 ggplot(df, aes(x = acousticness, y = loudness, colour = energy)) +
   geom_point() +
@@ -285,9 +296,9 @@ pca <- prcomp(df_numeric, scale. = TRUE)
 pca_summary <- summary(pca)
 
 pca_df <- as.data.frame(t(pca_summary$importance)) |>
-  rownames_to_column(var = "PC") |> 
-   gt() |> 
-   tab_header(title = "Principal component summary")
+  rownames_to_column(var = "PC") |>
+  gt() |>
+  tab_header(title = "Principal component summary")
 pca_df
 
 
@@ -332,7 +343,7 @@ ggplot(pca_loadings_df, aes(x = Variable, y = PC, fill = abs(Loading))) +
     fill = "Loading (abs)"
   )
 
-# Extra first 5 PCs, this explains about 70% of the variance
+# Extract first 5 PCs, this explains about 70% of the variance
 pca_data <- as.data.frame(pca$x[, 1:5])
 
 
@@ -348,10 +359,10 @@ screeplot <- fviz_nbclust(
 )
 
 screeplot +
-   labs(title = "Scree plot - Elbow Method for Optimal Clusters") +
-   theme_minimal() +
-   geom_line(aes(group = 1), linewidth = 1) +
-   geom_point(size = 3)
+  labs(title = "Scree plot - Elbow Method for Optimal Clusters") +
+  theme_minimal() +
+  geom_line(aes(group = 1), linewidth = 1) +
+  geom_point(size = 3)
 
 # No obvious 'elbow'
 
@@ -360,108 +371,119 @@ screeplot +
 
 # Create and visualise model for k = 2 to k = 5
 
-cluster_plots = list()
+cluster_plots <- list()
+
+set.seed(123)
 
 for (k in 2:10) {
-   
-   # km model
-   km.out <- kmeans(pca_data, centers = k, nstart = 25, iter.max = 100)
-   
-   # Visualisation
-   cluster_plot <- fviz_pca_ind(
-      pca,
-      habillage = km.out$cluster,
-      label = "none",
-      geom = "point",
-      palette = viridis(k)
-   ) +
-      ggtitle(paste("k = ", k)) +
-      theme(legend.position = "none")
-   
-   cluster_plots[[paste0("k", k)]] <- cluster_plot
-   
+  # km model
+  km.out <- kmeans(pca_data, centers = k, nstart = 25, iter.max = 100)
+
+  # Visualisation
+  cluster_plot <- fviz_pca_ind(
+    pca,
+    habillage = km.out$cluster,
+    label = "none",
+    geom = "point",
+    palette = viridis(k)
+  ) +
+    ggtitle(paste("k = ", k)) +
+    theme(legend.position = "none")
+
+  cluster_plots[[paste0("k", k)]] <- cluster_plot
 }
 
-cluster_plots$k2 + cluster_plots$k3 + cluster_plots$k4 + cluster_plots$k5
+cluster_plots$k2 + cluster_plots$k3 + cluster_plots$k4 + cluster_plots$k5 +
+   plot_annotation(title = "k-means clustering for k = 2 - 5")
 
 
 # Confirm correct number of clusters with silhouette plot
 silhouette_plot <- fviz_nbclust(pca_data,
-             kmeans,
-             method = "silhouette")
+  kmeans,
+  method = "silhouette"
+)
 
 silhouette_plot +
-   theme_minimal() +
-   labs(title = "Optimal number of clusters - silhouette method") +
-   geom_line(aes(group = 1), linewidth = 1) +
-   geom_point(size = 3)
-   
+  theme_minimal() +
+  labs(title = "Optimal number of clusters - silhouette method") +
+  geom_line(aes(group = 1), linewidth = 1) +
+  geom_point(size = 3)
+
 
 # This confirms that k = 2 is optimal.
-
+set.seed(123)
 km.out <- kmeans(pca_data, centers = 2, nstart = 25, iter.max = 100)
 
 fviz_pca_ind(
-   pca,
-   habillage = km.out$cluster,
-   label = "none",
-   geom = "point",
-   palette = viridis(2)) +
-   labs(title = "Song feature k-means clustering, with k = 2")
+  pca,
+  habillage = km.out$cluster,
+  label = "none",
+  geom = "point",
+  palette = viridis(2)
+) +
+  labs(title = "Song feature k-means clustering, with k = 2")
 
 
 # Numeric feature cluster analysis ----------------------------------------
 
 # Add cluster col to df_numeric
 df_clusters_num <- df_numeric |>
-   mutate(cluster = factor(km.out$cluster))
+  mutate(cluster = factor(km.out$cluster))
 
 # Sort col names alphabetically then produce gt table
 df_clusters_num <- df_clusters_num[, order(colnames(df_clusters_num))]
-df_clusters_num |> group_by(cluster) |>
-   summarise(across(everything(), median)) |> 
-   gt() |> 
-   tab_header("Numeric features for the two clusters") |> 
-   fmt_number(decimals = 3)
+df_clusters_num |>
+  group_by(cluster) |>
+  summarise(across(everything(), median)) |>
+  gt() |>
+  tab_header("Numeric features for the two clusters") |>
+  fmt_number(decimals = 3)
 
 # Pivot long for plotting numeric feature comparison
-df_long <- df_clusters_num %>% select(acousticness, energy, valence, cluster) |> 
-   pivot_longer(cols = -cluster, names_to = "feature", values_to = "value")
+df_long <- df_clusters_num %>%
+  select(acousticness, energy, valence, cluster) |>
+  pivot_longer(cols = -cluster, names_to = "feature", values_to = "value")
 
 ggplot(df_long, aes(x = cluster, y = value, fill = cluster)) +
-   geom_boxplot() +
-   facet_wrap(~ feature) +
-   theme_minimal() +
-   theme(panel.spacing = unit(1, "cm"),
-         legend.position = "none") +
-   labs(title = "Numeric feature distributions by cluster",
-        x = "Cluster",
-        y = "Value") +
-   scale_fill_viridis_d()
+  geom_boxplot() +
+  facet_wrap(~feature) +
+  theme_minimal() +
+  theme(
+    panel.spacing = unit(1, "cm"),
+    legend.position = "none"
+  ) +
+  labs(
+    title = "Numeric feature distributions by cluster",
+    x = "Cluster",
+    y = "Value"
+  ) +
+  scale_fill_viridis_d()
 
 
 # Categorical feature cluster analysis ------------------------------------
 
-df_clusters_cat <- df |> 
-   select(key, mode, time_signature) |> 
-   mutate(cluster = factor(km.out$cluster))
+df_clusters_cat <- df |>
+  select(key, mode, time_signature) |>
+  mutate(cluster = factor(km.out$cluster))
 
 df_clusters_cat_long <- df_clusters_cat |>
-   group_by(cluster) |> 
-   pivot_longer(cols = -cluster, names_to = "feature", values_to = "value")
+  group_by(cluster) |>
+  pivot_longer(cols = -cluster, names_to = "feature", values_to = "value")
 
-ggplot(df_clusters_cat_long,
-       aes(x = value, y = after_stat(prop), fill = cluster, group = cluster)) +
-   geom_bar(position = "dodge") +
-   facet_wrap(~ feature, scales = "free") +
-   scale_y_continuous(labels = scales::percent) +
-   theme_minimal() +
-   labs(
-      x = "Category value",
-      y = "Proportion",
-      title = "Proportion of categorical values by cluster"
-   ) +
-   scale_fill_viridis_d()
+ggplot(
+  df_clusters_cat_long,
+  aes(x = value, y = after_stat(prop), fill = cluster, group = cluster)
+) +
+  geom_bar(position = "dodge") +
+  facet_wrap(~feature, scales = "free") +
+  scale_y_continuous(labels = scales::percent) +
+  theme_minimal() +
+  labs(
+    x = "Category value",
+    y = "Proportion",
+    title = "Proportion of categorical features by cluster"
+  ) +
+  scale_fill_viridis_d()
 
 # No significant changes between the clusters.
 
@@ -471,69 +493,123 @@ ggplot(df_clusters_cat_long,
 # Pre-processing
 
 df_lyrics <- df |>
-   select(lyrics) |>
-   mutate(cluster = km.out$cluster) |> 
-   filter(lyrics != "")
+  select(lyrics) |>
+  mutate(cluster = km.out$cluster) |>
+  filter(lyrics != "")
 
 
-clean_lyrics <- function(lyric) {
-   
-   # Remove leading [" or [' 
-   lyric <- gsub("^\\[[\"']", "", lyric)
-   
-   # Remove trailing "] or ']
-   lyric <- gsub("[\"']\\]$", "", lyric)
-   
-      # Convert \n into a space
-      lyric <- gsub("\\\\n", " ", lyric)
-      
-      # Remove [Verse 1] etc.
-      lyric <- gsub("\\[.*?\\]", "", lyric)
-      
-      # Fix \\'
-      lyric <- gsub("\\\\'", "'", lyric)
-      
-      # Expand contractions
-      lyric <- replace_contraction(lyric)
-      
-      # Lowercasing
-      lyric <- tolower(lyric)
-      
-      # Remove punctuation
-      lyric <- removePunctuation(lyric)
-      
-      # Lemmatise words
-      lyric <- lemmatize_strings(lyric)
-            
-      
-      # Remove stopwords
-      lyric <- removeWords(lyric, stopwords("en"))
-      
-
-      return(lyric)
+clean_lyrics <- function(x) {
+  x |>
+    # remove leading [" or ['
+    str_remove("^\\[[\"']") |>
+    # remove trailing "]
+    str_remove("[\"']\\]$") |>
+    # convert \n to space
+    str_replace_all("\\\\n", " ") |>
+    # convert \xa0 to space
+    str_replace_all("\\\\xa0", " ") |>
+    # remove any [Verse], [Chorus], etc.
+    str_remove_all("\\[.*?\\]") |>
+    # fix \'
+    str_replace_all("\\\\'", "'") |>
+    # expand contractions
+    replace_contraction() |>
+    # lower-case
+    str_to_lower() |>
+    # remove punctuation
+    removePunctuation() |>
+    # remove numbers
+    removeNumbers() |>
+    # lemmatize
+    lemmatize_strings()
 }
 
+
 # Create new col with cleaned lyrics (this takes a long time to run).
-df_lyrics <- df_lyrics |> 
-   mutate(clean_lyrics = clean_lyrics(lyrics))
-
-# Filter out empty lyrics (caused by clean_lyrics function)
-
-df_lyrics_nonempty <- df_lyrics |> 
-   filter(clean_lyrics != "")
+df_lyrics <- df_lyrics |>
+  mutate(clean_lyrics = clean_lyrics(lyrics))
 
 
-# Create document term matrices
+# Create word df with tokenisation, remove stopwords in process, use only
+# word in dictionary.
+data("stop_words")
 
-lyrics_corpus <- Corpus(VectorSource(df_lyrics_nonempty$clean_lyrics))
-
-lyrics_dtm <- DocumentTermMatrix(lyrics_corpus)
-
-lyrics_dtm_tfidf <- DocumentTermMatrix(lyrics_corpus,
-                                       control = list(weighting = weightTfIdf))
-
-
+words_df <- df_lyrics |>
+  filter(lyrics != "") |>
+  unnest_tokens(word, clean_lyrics) |>
+  anti_join(stop_words, by = "word") |>
+  filter(word %fin% GradyAugmented) |>
+  filter(nchar(word) > 2)
 
 
+# Lyric visualisations ----------------------------------------------------
+
+# Top words for all songs
+word_count <- words_df |>
+  select(word, cluster) |>
+  group_by(word) |>
+  summarise(count = n()) |>
+  arrange(desc(count)) |>
+  # rotate about 85% of the words for the wordcloud
+  mutate(angle = 45 * sample(-2:2, n(),replace = TRUE, prob = c(1, 1, 6, 1, 1))
+         )
+
+ggplot(word_count[1:40, ], aes(
+  label = word,
+  size = count,
+  angle = angle,
+  color = factor(sample.int(40, 40, replace = TRUE))
+)) +
+  geom_text_wordcloud_area() +
+  scale_size_area(max_size = 50) +
+  scale_colour_viridis_d() +
+  theme_minimal() +
+  labs(
+    title = "40 most common lyrics from over 20,000 songs",
+    subtitle = "Lyrics have been filtered and lemmatised",
+    caption = "Data source: MusicOSet"
+  )
+
+# Top words in cluster 1 (higher energy)
+word_count_cluster_1 <- words_df |>
+  select(word, cluster) |>
+  filter(cluster == 1) |>
+  group_by(word) |>
+  summarise(count = n()) |>
+  arrange(desc(count))
+
+cluster1_words <- ggplot(word_count_cluster_1[1:40, ], aes(
+  label = word,
+  size = count,
+  color = factor(sample.int(40, 40, replace = TRUE))
+)) +
+  geom_text_wordcloud_area(shape = "square", eccentricity = 1) +
+  scale_size_area(max_size = 40) +
+  scale_colour_viridis_d() +
+  theme_minimal() +
+  labs(title = "Cluster 1")
+
+# Top words in cluster 2 (more acoustic)
+word_count_cluster_2 <- words_df |>
+  select(word, cluster) |>
+  filter(cluster == 2) |>
+  group_by(word) |>
+  summarise(count = n()) |>
+  arrange(desc(count))
+
+cluster2_words <- ggplot(word_count_cluster_2[1:40, ], aes(
+  label = word,
+  size = count,
+  color = factor(sample.int(40, 40, replace = TRUE))
+)) +
+  geom_text_wordcloud_area(shape = "square", eccentricity = 1) +
+  scale_size_area(max_size = 40) +
+  scale_colour_viridis_d() +
+  theme_minimal() +
+  labs(title = "Cluster 2")
+
+
+cluster1_words / cluster2_words +
+   plot_annotation(title = "40 most common lyrics, by cluster")
 
 
